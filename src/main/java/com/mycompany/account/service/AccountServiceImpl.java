a0e0e78894684592e99a1c3cc86b1f2409f0bf42
Colin But <colinbut@users.noreply.github.com>
2018-08-02 12:50:26 +0100

    making tests passed for create account and getting account methods

--- src/main/java/com/mycompany/account/service/AccountServiceImpl.java
+++ src/main/java/com/mycompany/account/service/AccountServiceImpl.java
@@ -6,28 +6,64 @@
 package com.mycompany.account.service;
 
 import com.mycompany.account.model.Account;
+import com.mycompany.account.repository.AccountRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountServiceImpl.class);
+
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Override
     public void createAccount(Account account) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        LOGGER.info("Creating account: {}", account);
+
+        accountRepository.saveAccount(account);
+
+        LOGGER.info("Created account: {}", account);
     }
 
     @Override
     public void updateAccount(Account account) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        LOGGER.info("Updating account: {}", account);
+
+        int accountId = account.getAccountId();
+
+        Account accountFromStore = accountRepository.findAccount(accountId);
+        if (accountFromStore == null) {
+            throw new RuntimeException("No existing account with accountId: + " + accountId + " exist");
+        }
+
+        LOGGER.debug("Found existing account from Store to update: {} ... Updating account data with: {}", accountFromStore, account);
+
+        accountRepository.saveAccount(account);
+
+        LOGGER.info("Updated account: {}", account);
     }
 
     @Override
     public void deleteAccount(int accountId) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        LOGGER.info("Deleting account with accountId: {}", accountId);
+        
+        Account accountFromStore = accountRepository.findAccount(accountId);
+        if (accountFromStore == null) {
+            throw new RuntimeException("No existing account with accountId: + " + accountId + " exist");
+        }
+
+        accountRepository.removeAccount(accountFromStore);
+
+        LOGGER.info("Removed account: {}", accountFromStore);
     }
 
     @Override
     public Account getAccount(int accountId) {
-        return null;
+        return accountRepository.findAccount(accountId);
     }
 }
