a228aae4074bfd5f42bc7eb9e8ec2662185f7206
Colin But <colinbut@users.noreply.github.com>
2018-08-02 11:05:01 +0100

    adding DTOs to match that of UserService

---
+++ src/main/java/com/mycompany/account/messaging/event/UserEventType.java
@@ -0,0 +1,12 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.account.messaging.event;
+
+public enum UserEventType {
+    CREATED,
+    UPDATED,
+    DELETED
+}
