8eedd0affc3fc9cf67ed792e9af8b3251a81240b
Colin But <colinbut@users.noreply.github.com>
2018-08-02 11:21:48 +0100

    model class

--- src/main/java/com/mycompany/account/model/Account.java
+++ src/main/java/com/mycompany/account/model/Account.java
@@ -5,7 +5,100 @@
  */
 package com.mycompany.account.model;
 
+import java.util.Date;
+
 public class Account {
     private int accountId;
+    
+    private String ssn;
+    private String forename;
+    private String surname;
+    private Date date_of_birth;
+
+    private String firstLineAddress;
+    private String secondLineAddress;
+    private String postCode;
+    private String city;
+    private String country;
+
+	public int getAccountId() {
+		return this.accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getSsn() {
+		return this.ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getForename() {
+		return this.forename;
+	}
+
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getDate_of_birth() {
+		return this.date_of_birth;
+	}
+
+	public void setDate_of_birth(Date date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+
+	public String getFirstLineAddress() {
+		return this.firstLineAddress;
+	}
+
+	public void setFirstLineAddress(String firstLineAddress) {
+		this.firstLineAddress = firstLineAddress;
+	}
+
+	public String getSecondLineAddress() {
+		return this.secondLineAddress;
+	}
+
+	public void setSecondLineAddress(String secondLineAddress) {
+		this.secondLineAddress = secondLineAddress;
+	}
+
+	public String getPostCode() {
+		return this.postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
 }
